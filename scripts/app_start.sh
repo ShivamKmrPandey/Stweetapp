d27619a5389ef7710ec6a29577e566b2e46ef026
ShivamKmrPandey <104768132+ShivamKmrPandey@users.noreply.github.com>
2022-05-03 12:10:56 +0530

    add

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "STWEETAPP" -- start
+# initial startup by running react-script "start", name process "marketing"
+# --watch watches and restarts if files change
+pm2 start ./node_modules/react-scripts/scripts/start.js --name "tweetapp" --watch
+
+# auto restart server if shut down
 pm2 startup
+
+# freeze process list for automatic respawn
 pm2 save
+
+# restart all processes - necessary to do this again?
 pm2 restart all
